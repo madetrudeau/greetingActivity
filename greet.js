23fadc62d7412e887ec7034a7df19065f000f42c
madtrudeau <112499812+madtrudeau@users.noreply.github.com>
2022-10-31 13:13:07 -0700

    Matched all quote marks to single tick

--- greet.js
+++ greet.js
@@ -1,18 +1,16 @@
 greet = (name) => {
     if (name === null){
-        return "Hello, there!"
-    }
-    if (name.isArray()) {
-        let nameArray = ['Hello '];
+    if (name.isArray) {
+        let nameArray = ['Hello'];
         for (let i = 0; i < name.length(); i++){
             nameArray.push(name[i])
         }
-        return 'Hello' + nameArray.join(', ')
+        return (nameArray.join())
     } 
-    if (name.toUpperCase() === name) {
-        return "HELLO " + name + "!";
+    if (name.toUpperCase === name) {
+        return 'HELLO ' + name + '!';
     }  
     else {
-        return "Hello, " + name;
+        return 'Hello, ' + name;
     }
 }
