7b38f2b7a0b96f82716fb0a9261327d2fbf02df7
madtrudeau <112499812+madtrudeau@users.noreply.github.com>
2022-10-31 12:29:30 -0700

    added uppercase logic

--- greet.js
+++ greet.js
@@ -1,6 +1,11 @@
 greet = (name) => {
     if (name === null){
         return "Hello, there!"
+    }
+    if (name.upper() === true) {
+        return "HELLO" + name;
+    } else {
+        
     }    
     else{
         return "Hello, " + name;
