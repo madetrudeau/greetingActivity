54419e86fae01ebb3c32f2321e597bd61dd06235
madtrudeau <112499812+madtrudeau@users.noreply.github.com>
2022-10-31 13:05:36 -0700

    Moved array testing before uppercase

--- greet.js
+++ greet.js
@@ -2,15 +2,15 @@ greet = (name) => {
     if (name === null){
         return "Hello, there!"
     }
-    if (name.toUpperCase() === name) {
-        return "HELLO " + name + "!";
-    } 
-    if (name.isArray) {
+    if (name.isArray()) {
         let nameArray = ['Hello '];
         for (let i = 0; i < name.length(); i++){
             nameArray.push(name[i])
         }
         return 'Hello' + nameArray.join(', ')
+    } 
+    if (name.toUpperCase() === name) {
+        return "HELLO " + name + "!";
     }  
     else {
         return "Hello, " + name;
